deb4acdb4d8773a93da87979b331e8485938983c
Jay Salmon <jaysal3@gmail.com>
2024-12-27 14:24:55 -0800

    Update mmwave_sensor.h

--- mmwave_sensor.h
+++ mmwave_sensor.h
@@ -15,7 +15,7 @@ class MmWaveSensor : public Component, public UARTDevice, public Sensor {
   Sensor *heart_rate_sensor = new Sensor();
 
   MmWaveSensor(UARTComponent *uart)
-      : Component(), UARTDevice(uart), sensor(&uart->get_stream()) {} 
+      : Component(), UARTDevice(uart), sensor(uart) {}  // Changed from get_stream() to uart directly
 
   void setup() override {
     ESP_LOGI("MmWaveSensor", "Initializing sensor...");
@@ -33,7 +33,7 @@ class MmWaveSensor : public Component, public UARTDevice, public Sensor {
     }
   }
 
-  void loop() override {
+  void loop() override {  // Changed from update() to loop()
     read_uart_data();
   }
 
@@ -45,7 +45,7 @@ class MmWaveSensor : public Component, public UARTDevice, public Sensor {
     }
   }
 
-  void update() override {
+  void update_sensors() {  // This method can be used to update sensor values if needed
     uint16_t presence = sensor.smHumanData(sensor.eHumanPresence);
     presence_sensor->publish_state(presence);
 
