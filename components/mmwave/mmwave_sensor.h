88414929a193245a47a2f8972b4edb135ddcc045
Jay Salmon <jaysal3@gmail.com>
2024-12-28 17:51:13 -0800

    Update mmwave_sensor.h

--- components/mmwave/mmwave_sensor.h
+++ components/mmwave/mmwave_sensor.h
@@ -1,31 +1,54 @@
-// mmwave_sensor.h
 #pragma once
 #include "esphome/components/sensor/sensor.h"
+#include "esphome/components/uart/uart.h"
 #include "esphome/core/component.h"
-#include <random>
 
 namespace esphome {
 namespace mmwave_sensor {
 
-class MMWaveSensor : public sensor::Sensor, public PollingComponent {
+class MMWaveSensor : public sensor::Sensor, public PollingComponent, public uart::UARTDevice {
  public:
+  MMWaveSensor(uart::UARTComponent *uart) : UARTDevice(uart) {}
+
   void setup() override;
   void loop() override;
   void update() override;
   void dump_config() override;
+  
+  void set_presence_sensor(sensor::Sensor *presence_sensor) { presence_sensor_ = presence_sensor; }
+  void set_movement_sensor(sensor::Sensor *movement_sensor) { movement_sensor_ = movement_sensor; }
+  void set_movement_range_sensor(sensor::Sensor *range_sensor) { movement_range_sensor_ = range_sensor; }
+  void set_breath_sensor(sensor::Sensor *breath_sensor) { breath_sensor_ = breath_sensor; }
+  void set_heart_sensor(sensor::Sensor *heart_sensor) { heart_sensor_ = heart_sensor; }
 
  protected:
-  bool is_sensor_ready() {
-    return true;  // For testing purposes
-  }
+  bool begin();
+  bool config_work_mode(uint8_t mode);
+  uint8_t get_work_mode();
+  bool config_led_light(uint8_t led, uint8_t state);
+  uint8_t get_led_light_state(uint8_t led);
+  bool sensor_reset();
   
-  void read_sensor_data();
+  uint16_t get_human_data(uint8_t type);
+  uint8_t get_heart_rate();
+  uint8_t get_breathe_value();
   
-  // Random number generation
-  std::random_device rd;
-  std::mt19937 gen{rd()};
-  std::uniform_real_distribution<float> dist{0.0f, 100.0f};
+  bool send_command(uint8_t control, uint8_t cmd, uint16_t len, uint8_t *send_data, uint8_t *ret_data);
+  uint8_t calculate_checksum(uint8_t len, uint8_t *buf);
+
+  sensor::Sensor *presence_sensor_{nullptr};
+  sensor::Sensor *movement_sensor_{nullptr};
+  sensor::Sensor *movement_range_sensor_{nullptr};
+  sensor::Sensor *breath_sensor_{nullptr};
+  sensor::Sensor *heart_sensor_{nullptr};
+  
+  static const uint8_t SLEEP_MODE = 0x02;
+  static const uint8_t HP_LED = 0x04;
+  
+  static const uint8_t HUMAN_PRESENCE = 0x00;
+  static const uint8_t HUMAN_MOVEMENT = 0x01;
+  static const uint8_t HUMAN_RANGE = 0x02;
 };
 
-}  // namespace mmwave_sensor
-}  // namespace esphome
+} // namespace mmwave_sensor
+} // namespace esphome
