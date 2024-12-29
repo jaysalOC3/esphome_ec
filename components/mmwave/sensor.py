1ede4e67d5ebcdc12187eca68baf276876d7a799
Jay Salmon <jaysal3@gmail.com>
2024-12-28 17:52:03 -0800

    Update sensor.py

--- components/mmwave/sensor.py
+++ components/mmwave/sensor.py
@@ -1,28 +1,85 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor
+from esphome.components import sensor, uart
 from esphome.const import (
     CONF_ID,
     UNIT_EMPTY,
-    ICON_EMPTY,
+    ICON_MOTION_SENSOR,
+    ICON_RADAR,
+    ICON_HEART_PULSE,
+    DEVICE_CLASS_EMPTY,
 )
 
-DEPENDENCIES = []
+DEPENDENCIES = ["uart"]
 AUTO_LOAD = ["sensor"]
 
 mmwave_sensor_ns = cg.esphome_ns.namespace("mmwave_sensor")
-MMWaveSensor = mmwave_sensor_ns.class_("MMWaveSensor", sensor.Sensor, cg.PollingComponent)
+MMWaveSensor = mmwave_sensor_ns.class_("MMWaveSensor", sensor.Sensor, cg.PollingComponent, uart.UARTDevice)
+
+CONF_PRESENCE = "presence"
+CONF_MOVEMENT = "movement"
+CONF_MOVEMENT_RANGE = "movement_range"
+CONF_BREATH_RATE = "breath_rate"
+CONF_HEART_RATE = "heart_rate"
 
 CONFIG_SCHEMA = (
     sensor.sensor_schema(
         MMWaveSensor,
         unit_of_measurement=UNIT_EMPTY,
-        icon=ICON_EMPTY,
-        accuracy_decimals=1,
+        icon=ICON_RADAR,
+        accuracy_decimals=0,
     )
+    .extend({
+        cv.Optional(CONF_PRESENCE): sensor.sensor_schema(
+            unit_of_measurement=UNIT_EMPTY,
+            icon=ICON_MOTION_SENSOR,
+            accuracy_decimals=0,
+        ),
+        cv.Optional(CONF_MOVEMENT): sensor.sensor_schema(
+            unit_of_measurement=UNIT_EMPTY,
+            icon=ICON_MOTION_SENSOR,
+            accuracy_decimals=0,
+        ),
+        cv.Optional(CONF_MOVEMENT_RANGE): sensor.sensor_schema(
+            unit_of_measurement=UNIT_EMPTY,
+            icon=ICON_RADAR,
+            accuracy_decimals=0,
+        ),
+        cv.Optional(CONF_BREATH_RATE): sensor.sensor_schema(
+            unit_of_measurement="BPM",
+            icon=ICON_HEART_PULSE,
+            accuracy_decimals=0,
+        ),
+        cv.Optional(CONF_HEART_RATE): sensor.sensor_schema(
+            unit_of_measurement="BPM",
+            icon=ICON_HEART_PULSE,
+            accuracy_decimals=0,
+        ),
+    })
     .extend(cv.polling_component_schema("60s"))
+    .extend(uart.UART_DEVICE_SCHEMA)
 )
 
 async def to_code(config):
     var = await sensor.new_sensor(config)
     await cg.register_component(var, config)
+    
+    if CONF_PRESENCE in config:
+        sens = await sensor.new_sensor(config[CONF_PRESENCE])
+        cg.add(var.set_presence_sensor(sens))
+        
+    if CONF_MOVEMENT in config:
+        sens = await sensor.new_sensor(config[CONF_MOVEMENT])
+        cg.add(var.set_movement_sensor(sens))
+        
+    if CONF_MOVEMENT_RANGE in config:
+        sens = await sensor.new_sensor(config[CONF_MOVEMENT_RANGE])
+        cg.add(var.set_movement_range_sensor(sens))
+        
+    if CONF_BREATH_RATE in config:
+        sens = await sensor.new_sensor(config[CONF_BREATH_RATE])
+        cg.add(var.set_breath_sensor(sens))
+        
+    if CONF_HEART_RATE in config:
+        sens = await sensor.new_sensor(config[CONF_HEART_RATE])
+        cg.add(var.set_heart_sensor(sens))
