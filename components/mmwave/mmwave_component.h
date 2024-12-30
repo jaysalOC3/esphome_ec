2832e7795ef5c72ddc639cb0925773476fadf623
Jay Salmon <jaysal3@gmail.com>
2024-12-30 18:44:07 +0000

    Refactor MMWaveComponent: reorganize class structure and update command constants

--- components/mmwave/mmwave_component.h
+++ components/mmwave/mmwave_component.h
@@ -1,3 +1,4 @@
+// mmwave_component.h
 #pragma once
 
 #include "esphome/core/component.h"
@@ -7,24 +8,22 @@ namespace esphome {
 namespace mmwave_ns {
 
 class MMWaveComponent : public uart::UARTDevice, public Component {
- public:
-  // Constants for commands and control bytes
-  static const uint8_t CMD_WHITE = 0x00;  // Initial state of the command parser
-  static const uint8_t CMD_HEAD = 0x01;   // State after receiving the header byte (0x53)
-  // ... add more states as needed ...
+public:
+    MMWaveComponent(uart::UARTDevice *parent) : uart::UARTDevice(parent) {}
+    
+    void setup() override;
+    void loop() override;
+    void dump_config() override;
+    uint8_t begin();
+    uint8_t getData(uint8_t con, uint8_t cmd, uint16_t len, uint8_t *senData, uint8_t *retData);
+    uint8_t sumData(uint8_t len, uint8_t *buf);
 
-  static const uint32_t TIME_OUT = 5000;  // Timeout for communication in milliseconds
-
-  uint8_t begin(void);
-  uint8_t getData(uint8_t con, uint8_t cmd, uint16_t len, uint8_t *senData,
-                  uint8_t *retData);  // Declaration of getData()
-  void setup() override;
-  void loop() override;
-  void dump_config() override;
-
- private:
-  uint8_t sumData(uint8_t len, uint8_t *data);
+private:
+    static const size_t BUFFER_SIZE = 10;
+    static const uint8_t CMD_WHITE = 0;
+    static const uint8_t CMD_HEAD = 1;
+    static const uint32_t TIME_OUT = 1000;
 };
 
-}  // namespace mmwave_component
-}  // namespace esphome
+} // namespace mmwave_ns
+} // namespace esphome
